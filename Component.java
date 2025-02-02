16b3fcb2afc3448ef1638183c702f5c0b24203e0
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-02-02 18:47:38 +0000

    first commit - setup

---
+++ Component.java
@@ -0,0 +1,9 @@
+// file2995195
+
+public interface Component {
+    public String getName();
+    public int getSize();
+    public int getCount();
+    public String display(String prefix);
+    public Component search(String name);
+    }
---
+++ File.java
@@ -0,0 +1,36 @@
+// file2995195
+
+// Leaf - Individual File.
+public class File implements Component {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCount'");
+    }
+
+    @Override
+    public String display(String prefix) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'display'");
+    }
+
+    @Override
+    public Component search(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+    
+}
